842636f834b9837d4b263693af218d9ca3abdb16
Lucas Gaspar <50829518+LucasMGaspar@users.noreply.github.com>
2025-07-22 15:46:21 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
       'http://localhost:3001',
       'http://localhost:3002',
       'http://127.0.0.1:3001',
+      'https://gym-tracke-front-up.vercel.app',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
